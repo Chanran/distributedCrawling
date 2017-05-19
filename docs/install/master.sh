2cab258f9a7458aa44490c6a7c488f2f4db3561b
blue <rong650751@163.com>
2017-05-19 11:57:54 +0800

    完善master主机脚本

--- docs/install/master.sh
+++ docs/install/master.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 echo "650751" | sudo -S cp /etc/apt/sources.list /etc/apt/sources.list_backup
-sudo mkdir -p /home/blue
-cd /home/blue
-sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse
-deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse" > sources.list
+sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-backports main restricted universe multiverse" > sources.list
 sudo mv ./sources.list /etc/apt/
-sudo apt update
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-sudo dpkg-reconfigure locales
-sudo apt install -y python-pip
-sudo apt install -y git
-sudo pip install scrapy
-sudo pip install scrapy-redis
+sudo apt-get update
+#export LC_ALL="en_US.UTF-8"
+#export LC_CTYPE="en_US.UTF-8"
+#sudo dpkg-reconfigure locales
+sudo apt-get install -y python-pip
+sudo apt-get install -y git
+sudo apt-get install -y python-lxml
+sudo apt-get install -y python-twisted-web
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scrapy
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple scrapy-redis
 sudo mkdir -p /web/python
+sudo chown -R blue:blue /web/python
 sudo chmod -R 755 /web/python
 cd /web/python
 git clone https://github.com/Chanran/distributedCrawling.git
