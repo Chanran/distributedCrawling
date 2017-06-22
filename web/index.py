1eceeec82eda0b27b40ee59a6eab66e113a0ca9f
blue <rong650751@163.com>
2017-06-22 23:55:32 +0800

    构建接收爬取url的python 网站

--- web/index.py
+++ web/index.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from bottle import route, run
+from bottle import route, run, static_file, error, request
 
 
-@route('/index', methods=['GET'])
+@route('/', method='GET')
 def index():
-    return 'hi'
+    return static_file('index.html', root='/home/blue/PycharmProjects/DistributedSpider/web')
+
+
+@route('/urls', method='POST')
+def handle_urls():
+    urls = request.forms.get('urls')
+    return "success"
+
+
+@error(404)
+def error404(error):
+    return 'route error, please try the right route.\n'
 
 run(host='localhost', port=9001)
 
